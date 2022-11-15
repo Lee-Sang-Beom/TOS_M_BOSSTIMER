88131966c8badebcc8ca3c66f614800cccd490b4
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-15 23:01:51 +0900

    add func : createUser(add require displayname), loginUser

--- Component/LoginTemplate.tsx
+++ Component/LoginTemplate.tsx
@@ -44,7 +44,21 @@ export default function LoginTemplate() {
         event.preventDefault();
 
         try {
-            const data = await signInWithEmailAndPassword(auth, email, password);
+            if (newAccount) {
+                // create Account
+                const data = await createUserWithEmailAndPassword(auth, email, password)
+                    .then(({ user }) => {
+                        setDoc(
+                            doc(db, "userInfo", user.uid),
+                            { displayName },
+                            { merge: true }
+                        );
+                    });
+            }
+            else {
+                const data = await signInWithEmailAndPassword(auth, email, password);
+
+            }
         } catch (error: any) {
             setError(getErrorText(error));
         }
@@ -61,20 +75,21 @@ export default function LoginTemplate() {
         <div className="lg:w-3/6 md:w-1/2 bg-gray-100 rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0">
             <h2 className="text-gray-900 text-lg font-medium title-font mb-5">{!newAccount ? `로그인` : `회원가입`}</h2>
             <Form onSubmit={onSubmit}>
-                <LoginForm email={email} setEmail={setEmail} password={password} setPassWord={setPassWord}/>
-                {newAccount && 
-                <>
-                <Form.Field>
-                    <div className="relative mb-4">
-                        <Label className="leading-7 text-sm text-gray-600">닉네임</Label>
-                        <Input
-                            type="text"
-                            value={displayName}
-                            onChange={(e) => setDisplayName(e.target.value)}
-                            className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
-                    </div>
-                </Form.Field>
-                </>}
+                <LoginForm email={email} setEmail={setEmail} password={password} setPassWord={setPassWord} />
+                {newAccount &&
+                    <>
+                        <Form.Field>
+                            <div className="relative mb-4">
+                                <Label className="leading-7 text-sm text-gray-600">닉네임</Label>
+                                <input
+                                    type="text"
+                                    required
+                                    value={displayName}
+                                    onChange={(e) => setDisplayName(e.target.value)}
+                                    className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            </div>
+                        </Form.Field>
+                    </>}
                 <div className="flex items-center">
                     <button className="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">{!newAccount ? `로그인` : `회원가입`}</button>
                 </div>
--- Component/sign.tsx
+++ Component/sign.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import LoginTemplate from "./LoginTemplate";
+import test from "../public/img/test.jpg";
 
 export default function Sign() {
 
@@ -6,12 +8,12 @@ export default function Sign() {
         <div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto flex flex-wrap items-center justify-center">
-                    <div className="w-1/2 md:pr-16 lg:pr-0 pr-0">
+                    <div className="w-1/2 md:pr-16 lg:pr-0 pr-0 flex flex-col items-center">
                         <h1 className="title-font font-medium text-3xl text-gray-900 text-center">{`Tree Of Savior M - BossTimer`}</h1>
-                        <p className="leading-relaxed mt-4 text-center">라이마</p>
+                        <Image className="mt-4" src={test} width={300} height={300} alt="what?"/>
+                        <p className="leading-relaxed mt-4 text-center">본 사이트는 니들의 개인정보따위 관심없으니 안심하라구~</p>
                     </div>
-                    <LoginTemplate />
-                    
+                    <LoginTemplate />   
                 </div>
             </section>
         </div>
