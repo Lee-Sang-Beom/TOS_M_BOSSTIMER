493742d25e1b6db8ef236ca331b61c4b5bf6ae8c
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-15 13:43:36 +0900

    install next.js, firebase, bootstrap, tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Reset } from 'styled-reset'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+  <Reset />
+  <Component {...pageProps} />
+  </>
 }
