89c1aa37830291b6b0165e0d999705bbe1d492e9
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-16 11:15:44 +0900

    add func : logout(home)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Reset } from 'styled-reset'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="container">
+      <Reset/>
       <Component {...pageProps} />
     </div>
   )
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,14 +1,32 @@
 import { authService as auth } from "../firebaseConfig";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
+import { Icon, Label } from "semantic-ui-react";
 
 export default function Home() {
 
+  const router = useRouter();
+  const onLogOutClick = () => {
+    auth.signOut();
+    router.replace("/");
+  };
+
   return (
-    <h1>
-      Home
-       - 로그아웃 기능 추가예정
-       - 에피소드 별 타이머 확인 이동 기능 추가 예정
-    </h1>
+    <>
+      <button
+        className="text-sm w-1/3
+       mt-4 createAccount_btn bg-slate-600 text-white"
+        onClick={onLogOutClick}>
+        {`로그아웃하기`}
+      </button>
+      <h1>
+        Home 화면입니다.
+      </h1>
+      <ol>
+        <li>로그아웃 기능 구현 완료</li>
+        <li>에피소드 별 타이머 확인 이동 기능 추가 예정</li>
+      </ol>
+    </>
+
   )
 }
