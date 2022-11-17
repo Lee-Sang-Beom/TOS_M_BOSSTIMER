549b9eaf7046b595900260c464a8965917725687
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-18 00:00:37 +0900

    add episode compont (ep03~ep05)

--- Component/episodeSection.tsx
+++ Component/episodeSection.tsx
@@ -1,5 +1,8 @@
 import Ep01 from "./EP/episode1"
 import Ep02 from "./EP/episode2"
+import Ep03 from "./EP/episode3"
+import Ep04 from "./EP/episode4"
+import Ep05 from "./EP/episode5"
 
 export default function EpisodeSection() {
 
@@ -8,6 +11,9 @@ export default function EpisodeSection() {
             <div className="px-5 py-24 mx-auto">
                 <Ep01/>
                 <Ep02/>
+                <Ep03/>
+                <Ep04/>
+                <Ep05/>
             </div>
         </section>
     )
--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,180 @@ export const ep01BossListAtom = atom({
   ]
 });
 
-
 export const ep02BossListAtom = atom({
   key: "ep02BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '네프리타스 벼랑',
+      bossName: '강인한 쓰론위버',
+      time: '2'
+    },{
+      id : 2,
+      area: '테넷 화원',
+      bossName: '강인한 네펜더스',
+      time: '2'
+    },{
+      id : 3,
+      area: '테넷 성당 지하 1층',
+      bossName: '비겁한 사이클롭스',
+      time: '2'
+    },{
+      id : 4,
+      area: '테넷 성당 지상 1층',
+      bossName: '강인한 말렛와이번',
+      time: '2'
+    },{
+      id : 5,
+      area: '테넷 성당 지상 2층',
+      bossName: '강인한 네크로벤터',
+      time: '2'
+    }
+  ]
+});
+
+export const ep03BossListAtom= atom({
+  key: "ep03BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '다단 숲',
+      bossName: '강인한 일티스워트',
+      time: '2'
+    },{
+      id : 2,
+      area: '노바하 공회소',
+      bossName: '비겁한 머미가스트',
+      time: '2'
+    },{
+      id : 3,
+      area: '노바하 별관 ',
+      bossName: '강인한 벨니아몽키',
+      time: '2'
+    },{
+      id : 4,
+      area: '노바하 본원',
+      bossName: '분노한 데스위버',
+      time: '4'
+    }
+  ]
+});
+
+export const ep04BossListAtom= atom({
+  key: "ep04BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '코발트 숲',
+      bossName: '비겁한 클라이멘',
+      time: '2'
+    },{
+      id : 2,
+      area: '셉티니 골',
+      bossName: '강인한 모스템',
+      time: '2'
+    },{
+      id : 3,
+      area: '펠케 신전터',
+      bossName: '강인한 투투',
+      time: '2'
+    },{
+      id : 4,
+      area: '압셈타 수원지',
+      bossName: '분노한 히드라',
+      time: '4'
+    }
+  ]
+});
+
+export const ep05BossListAtom= atom({
+  key: "ep05BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '델무어 소작촌',
+      bossName: '비겁한 리버피드',
+      time: '2'
+    },{
+      id : 2,
+      area: '델무어 장원',
+      bossName: '강인한 그라이넨더',
+      time: '2'
+    },{
+      id : 3,
+      area: '델무어 외성',
+      bossName: '분노한 붉은 래밴자드',
+      time: '4'
+    }
+  ]
+});
+
+export const ep06BossListAtom= atom({
+  key: "ep06BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '네프리타스 벼랑',
+      bossName: '강인한 쓰론위버',
+      time: '2'
+    },{
+      id : 2,
+      area: '테넷 화원',
+      bossName: '강인한 네펜더스',
+      time: '2'
+    },{
+      id : 3,
+      area: '테넷 성당 지하 1층',
+      bossName: '비겁한 사이클롭스',
+      time: '2'
+    },{
+      id : 4,
+      area: '테넷 성당 지상 1층',
+      bossName: '강인한 말렛와이번',
+      time: '2'
+    },{
+      id : 5,
+      area: '테넷 성당 지상 2층',
+      bossName: '강인한 네크로벤터',
+      time: '2'
+    }
+  ]
+});
+
+export const ep07BossListAtom= atom({
+  key: "ep07BossListAtom", // unique ID
+  default: [
+    {
+      id : 1,
+      area: '네프리타스 벼랑',
+      bossName: '강인한 쓰론위버',
+      time: '2'
+    },{
+      id : 2,
+      area: '테넷 화원',
+      bossName: '강인한 네펜더스',
+      time: '2'
+    },{
+      id : 3,
+      area: '테넷 성당 지하 1층',
+      bossName: '비겁한 사이클롭스',
+      time: '2'
+    },{
+      id : 4,
+      area: '테넷 성당 지상 1층',
+      bossName: '강인한 말렛와이번',
+      time: '2'
+    },{
+      id : 5,
+      area: '테넷 성당 지상 2층',
+      bossName: '강인한 네크로벤터',
+      time: '2'
+    }
+  ]
+});
+
+export const ep08BossListAtom= atom({
+  key: "ep08BossListAtom", // unique ID
   default: [
     {
       id : 1,
