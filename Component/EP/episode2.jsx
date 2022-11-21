5b78cfdc0f0e48f3156c6f4381934253923c5db1
Lee-Sang-Beom <tkdqja975@naver.com>
2022-11-21 15:51:00 +0900

    notify login screen : change div width : width < 768px & width > 768px

--- Component/EP/episode2.jsx
+++ Component/EP/episode2.jsx
@@ -19,7 +19,7 @@ export default function Ep02() {
     const [nextSecond, setNextSecond] = useState([]);
     const [bossData, setBossData] = useRecoilState(ep02BossListAtom);
 
-    // episode1의 collection Name
+    // episode2의 collection Name
     const q = query(collection(dbService, "episode2"));
 
     // 설정된 다음 시간을 받아오는 부분
--- Component/sign.tsx
+++ Component/sign.tsx
@@ -7,8 +7,8 @@ export default function Sign() {
         <div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5 py-24 mx-auto flex flex-wrap items-center justify-center">
-                    <div className="w-1/2 md:pr-16 lg:pr-0 pr-0 flex flex-col items-center">
-                        <h1 className="animate__animated animate__fadeInDown mb-3">모두가 함께 만들어나가는 시간</h1>
+                    <div className="w-full md:w-1/2 md:pr-16 lg:pr-0 pr-0 flex flex-col items-center">
+                        <h1 className="animate__animated animate__fadeInDown mb-3 ">모두가 함께 만들어나가는 시간</h1>
                         <h1 className="title-font font-bold text-3xl text-gray-900 text-center animate__animated animate__fadeInDownBig">{`Tree Of Savior M`}</h1>
                         <h3 className="title-font font-bold text-base animate__animated animate__flipInX"> BossTimer </h3>
                         <Animation/>
